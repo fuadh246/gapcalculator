6249e5f0772b89670715833d9fd58f9e65119ef5
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-02-06 13:12:16 -0500

    with somr rm

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-//var class_name = document.getElementById('class');
-//var grade = document.getElementById('grade');
-//var credit = document.getElementById('credit');
-//var submit = document.getElementById('submit');
+var class_name = document.getElementById('class');
+var grade = document.getElementById('grade');
+var credit = document.getElementById('credit');
+var submit = document.getElementById('submit');
 var add_more = document.getElementById('add_more');
-//var remove = document.getElementById('remove');
+var remove = document.getElementById('remove');
 var input_div = document.getElementById('input_div');
-//var class_name = document.getElementById('class_name');
 
 add_more.onclick = function(){
-    var div = document.createElement('input_div');
-    div.id='input_div';
-    div.innerHTML = 'Hi there!';
-    document.body.appendChild(div);
+    
+    var newclass_name = document.createElement('input');
+    newclass_name.innerText='<label>Class name:</label>'
+    newclass_name.setAttribute('type','text');
+    newclass_name.setAttribute('class','class_name');
+    newclass_name.innerHTML='<label>Class name:</label>'
+    input_div.appendChild(newclass_name);
+    var newclass_name = document.createElement('select');
+    newclass_name.innerText='<label>Class name:</label>'
+    newclass_name.setAttribute('type','text');
+    newclass_name.setAttribute('class','class_name');
+    newclass_name.innerHTML='<label>Class name:</label>'
+    input_div.appendChild(newclass_name);var newclass_name = document.createElement('input');
+    newclass_name.innerText='<label>Class name:</label>'
+    newclass_name.setAttribute('type','text');
+    newclass_name.setAttribute('class','class_name');
+    newclass_name.innerHTML='<label>Class name:</label>'
+    input_div.appendChild(newclass_name);
+    
 }
