fa358af86bab76877e419ee6fa6fdf9a699c9728
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-02-06 13:44:42 -0500

    restarting with static :(

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-var class_name = document.getElementById('class');
+//var class_name = document.getElementById('class');
 var grade = document.getElementById('grade');
-var credit = document.getElementById('credit');
-var submit = document.getElementById('submit');
+//var credit = document.getElementById('credit');
+//var submit = document.getElementById('submit');
 var add_more = document.getElementById('add_more');
-var remove = document.getElementById('remove');
+//var remove = document.getElementById('remove');
 var input_div = document.getElementById('input_div');
+//var class_name = document.getElementById('class_name');
 
 add_more.onclick = function(){
-    
+    var newclass_name = document.createElement('label');
+    input_div.appendChild(newclass_name);
+    newclass_name.innerHTML='<label >Class name: </label>'
+
     var newclass_name = document.createElement('input');
     newclass_name.innerText='<label>Class name:</label>'
     newclass_name.setAttribute('type','text');
     newclass_name.setAttribute('class','class_name');
     newclass_name.innerHTML='<label>Class name:</label>'
     input_div.appendChild(newclass_name);
-    var newclass_name = document.createElement('select');
-    newclass_name.innerText='<label>Class name:</label>'
-    newclass_name.setAttribute('type','text');
-    newclass_name.setAttribute('class','class_name');
-    newclass_name.innerHTML='<label>Class name:</label>'
-    input_div.appendChild(newclass_name);var newclass_name = document.createElement('input');
-    newclass_name.innerText='<label>Class name:</label>'
-    newclass_name.setAttribute('type','text');
-    newclass_name.setAttribute('class','class_name');
-    newclass_name.innerHTML='<label>Class name:</label>'
+
+    var newclass_name = document.createElement('label');
     input_div.appendChild(newclass_name);
-    
+    newclass_name.innerHTML='<label >Grade </label>'
+
+    var newclass_name = document.createElement('select');
+    newclass_name.value='A';
+    newclass_name.value='B';
+    newclass_name.setAttribute('class','grade');
+    input_div.appendChild(newclass_name);
+
+    var newclass_name = document.createElement('label');
+    input_div.appendChild(newclass_name);
+    newclass_name.innerHTML='<label >Credit/Hours </label>'
+
+    var newclass_name = document.createElement('input');
+    newclass_name.innerHTML='<label>Class name:</label>'
+    newclass_name.setAttribute('type','number');
+    newclass_name.setAttribute('class','class_name');
+    input_div.appendChild(newclass_name);
+
+    var newclass_name = document.createElement('br');
+    input_div.appendChild(newclass_name);
+    newclass_name.innerHTML='<br>'
 }
