bfdb511f6349d802e4a23255f8c82080159a7264
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-02-06 04:04:27 -0500

    last post for the 🌃

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var class_name = document.getElementById('class');
-var grade = document.getElementById('grade');
-var credit = document.getElementById('credit');
-var submit = document.getElementById('submit');
+//var class_name = document.getElementById('class');
+//var grade = document.getElementById('grade');
+//var credit = document.getElementById('credit');
+//var submit = document.getElementById('submit');
 var add_more = document.getElementById('add_more');
-var remove = document.getElementById('remove');
+//var remove = document.getElementById('remove');
 var input_div = document.getElementById('input_div');
-var class_name = document.getElementById('class_name');
+//var class_name = document.getElementById('class_name');
 
 add_more.onclick = function(){
-    var newinput_div = document.createElement('div');
-    newinput_div.setAttribute('class','input_div');
-    document.body.appendChild(newinput_div);
+    var div = document.createElement('input_div');
+    div.id='input_div';
+    div.innerHTML = 'Hi there!';
+    document.body.appendChild(div);
 }
