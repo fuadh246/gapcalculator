16839af5b06fffef0938af7fae0ac4d8ace9907c
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-02-06 21:23:09 -0500

    all most done , it's working now testing time

--- script.js
+++ script.js
@@ -22,20 +22,95 @@ submit.onclick = function(){
     var credit3 = document.getElementById('credit3').value;
     var credit4 = document.getElementById('credit4').value;
     var credit5 = document.getElementById('credit5').value;
-    
-    var sum = 0.0;
-    if(grade1 == 'A' || grade2 == 'A' || grade3 == 'A' || grade4 == 'A' || grade5 == 'A'){
-        sum += 4;
-    }
-    if(grade1 == 'B' || grade2 == 'B' || grade3 == 'B' || grade4 == 'B' || grade5 == 'B'){
-        sum += 3;
-    }
-    if(grade1 == 'C' || grade2 == 'C' || grade3 == 'C' || grade4 == 'C' || grade5 == 'C'){
-        sum += 2;
-    }
-    if(grade1 == 'D' || grade2 == 'D' || grade3 == 'D' || grade4 == 'D' || grade5 == 'D'){
-        sum += 1;
-    }
+    var grade1_value;
 
-    alert(sum);
+    var sumofcredit = (Number(credit1)+Number(credit2)+Number(credit3)+Number(credit4)+Number(credit5));
+
+
+    if(grade1 == 'A' ){
+        grade1_value = 4;
+    }else if(grade1 == 'B' ){
+        grade1_value = 3;
+    }else if(grade1 == 'C' ){
+        grade1_value = 2;
+    }else if(grade1 == 'D' ){
+        grade1_value = 1;
+    }
+    if(grade2 == 'A' ){
+        var grade2_value= 4;
+    }else if(grade2 == 'B' ){
+        var grade2_value = 3;
+    }else if(grade2 == 'C' ){
+        var grade2_value = 2;
+    }else if(grade2 == 'D' ){
+        var grade2_value = 1;
+    }
+    if(grade3 == 'A' ){
+        var grade3_value = 4;
+    }else if(grade3 == 'B' ){
+        var grade3_value = 3;
+    }else if(grade3 == 'C' ){
+        var grade3_value = 2;
+    }else if(grade3 == 'D' ){
+        var grade3_value = 1;
+    }
+    if(grade4 == 'A' ){
+        var grade4_value = 4;
+    }else if(grade4 == 'B' ){
+        var grade4_value = 3;
+    }else if(grade4 == 'C' ){
+        var grade4_value = 2;
+    }else if(grade4 == 'D' ){
+        var grade4_value = 1;
+    }
+    if(grade5 == 'A' ){
+        var grade5_value = 4;
+    }else if(grade5 == 'B' ){
+        var grade5_value = 3;
+    }else if(grade5 == 'C' ){
+        var grade5_value = 2;
+    }else if(grade5 == 'D' ){
+        var grade5_value = 1;
+    }
+    //alert(grade1_value);
+    //alert(credit1);
+
+    if(grade1 == '' || credit1 == null){
+       var sumofgrade1 = 0;
+    }else{
+        var sumofgrade1 =+ Number(grade1_value*credit1);
+    }
+    if(grade2 == '' || credit2 == null){
+        var sumofgrade2 =0;
+    }else{
+        var sumofgrade2 = Number(grade2_value*credit2);
+    }
+    if(grade3 == '' || credit3 == null){
+        var sumofgrade3 = 0;
+    }else{
+        var sumofgrade3 =+ Number(grade3_value*credit3);
+    }
+    if(grade4 == '' || credit4 == null){
+        var sumofgrade4 = 0;
+    }else{
+        var sumofgrade4 =+ Number(grade4_value*credit4);
+    }
+    if(grade5 == '' || credit5 == null){
+        var sumofgrade5 = 0;
+    }else{
+        var sumofgrade5 =+ Number(grade5_value*credit5);
+    }
+    
+    
+    /*sumofgrade =+ Number(grade2_value*credit2);
+    sumofgrade =+ Number(grade3_value*credit3);
+    sumofgrade =+ Number(grade4_value*credit4);
+    sumofgrade =+ Number(grade5_value*credit5);
+    */
+    var sumofgrade = Number(sumofgrade1+sumofgrade2+sumofgrade3+sumofgrade4+sumofgrade5);
+    alert(sumofgrade);
+
+    var gpa = (sumofgrade)/sumofcredit;
+    result.innerHTML = gpa;
+
 }
